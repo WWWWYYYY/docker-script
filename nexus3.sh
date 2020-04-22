c08ae874d18cee682bda6ad766beafe088ef52fd
WWWWYYYY <394489664@qq.com>
2020-04-22 18:04:25 +0800

    Update nexus3.sh
    修改脚本

--- nexus3.sh
+++ nexus3.sh
@@ -1,3 +1,7 @@
 docker volume create nexus
-docker run -d --name nexus3 --restart=always -p 8001:8081 -p 5000:5000 -v nexus:/nexus-data sonatype/nexus3
+docker run -d --name nexus3 --restart=always -p 8001:8081 -p 5000:5000 -v nexus:/nexus-data -u root sonatype/nexus3
+
+
+# mkdir -p /docker_data/nexus3
+#docker run -d --name nexus3 --restart=always -p 8001:8081 -p 5000:5000 -v /docker_data/nexus3:/nexus-data -u root sonatype/nexus3
 #浏览器访问ip:8001
